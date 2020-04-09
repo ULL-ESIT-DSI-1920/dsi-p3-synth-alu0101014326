b044dc61bed53d98f7d2955430de4b42bd237008
dkuroi <kaerit@hotmail.com>
2020-04-09 17:37:26 +0100

    it2

--- src/js/Conversation.js
+++ src/js/Conversation.js
@@ -3,25 +3,57 @@ export default class Conversation {
         this.box = box;
     }
     addMessage(array) {
-        this.msg = new SpeechSynthesisUtterance();
         array.map((dialog,i) => {
-            
-            this.entirely(dialog, dialog.text, i);           
+            if(dialog.mode == "entirely") this.entirely(dialog, dialog.text, i);  
+            if(dialog.mode == "ltl") this.letterToLetter(dialog, dialog.text, i); 
+            if(dialog.mode == "wtw") this.wordToWord(dialog, dialog.text, i);         
         });
     }
     
     entirely(dialog, text, i) {
         let msg = new SpeechSynthesisUtterance();
-            msg.text = text;
-            msg.lang = dialog.author.msg.lang;
-            msg.pitch = dialog.author.msg.pitch;
-            msg.rate = dialog.author.msg.rate;
-            msg.onstart = () => {
-                this.box.innerHTML += `<div class="dialog" style="color: ${dialog.author.color}">${dialog.author.name}: ${text}</div>`;
-                dialog.author = 1;
-            };
-            speechSynthesis.speak(msg);            
+        msg.text = text;
+        msg.lang = dialog.author.msg.lang;
+        msg.pitch = dialog.author.msg.pitch;
+        msg.rate = dialog.author.msg.rate;
+        msg.onstart = () => {
+            this.box.innerHTML += `<div class="dialog" style="color: ${dialog.author.color}">${dialog.author.name}: ${text}</div>`;
+        };
+        speechSynthesis.speak(msg);            
     }
-    wordToWord() {}
-    letterToLetter() {}
+    wordToWord(dialog, text, i) {
+        let msg = new SpeechSynthesisUtterance();
+        
+        msg.lang = dialog.author.msg.lang;
+        msg.pitch = dialog.author.msg.pitch;
+        msg.rate = dialog.author.msg.rate;
+        msg.text = text;
+
+        msg.onstart = () => this.box.innerHTML += `<div class="dialog" style="color: ${dialog.author.color}">${dialog.author.name}: <div id="fill${i}" style="display: inline-block"></div>  </div></div>`;
+        msg.onboundary = (event) => document.querySelector(`#fill${i}`).innerHTML = text.substring(0, event.charIndex+event.charLength+1);
+        
+        speechSynthesis.speak(msg);
+    }
+
+    letterToLetter(dialog, text, i) {
+        let msg = new SpeechSynthesisUtterance();
+        
+        msg.lang = dialog.author.msg.lang;
+        msg.pitch = dialog.author.msg.pitch;
+        msg.rate = dialog.author.msg.rate;
+        msg.text = text;
+
+        msg.onstart = () => {
+            this.box.innerHTML += `<div class="dialog" style="color: ${dialog.author.color}">${dialog.author.name}: <div id="fill${i}" style="display: inline-block"></div>  </div></div>`;
+        };
+ 
+        msg.onboundary = (event) => {
+            
+            console.log(event.name, event.charIndex, event.charLength, event.elapsedTime);
+            document.querySelector(`#fill${i}`).innerHTML = text.substring(0, event.charIndex+event.charLength+1);
+        };
+        speechSynthesis.speak(msg);
+    }
+
+    soundEffect() {}
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,6 @@
 import Profile from './Profile.js';
 import Conversation from './Conversation.js';
 
-console.log(document.querySelector('#curtain > h1'));
-
 const p1 = new Profile("Kaerit", {lang: "nb-NO", rate: 2, pitch: 2, color: "#ffa500", volume: 1});
 const p2 = new Profile("Keko", {lang: "it-IT", rate: 0.1, pitch: 0.1, color: "#00fff3", volume: 1});
 const p3 = new Profile("Nun", {lang: "es", rate: 1, pitch: 1, color: "#9d00ff", volume: 1});
@@ -16,29 +14,16 @@ document.querySelector('#curtain > h1').addEventListener('click', () => {
 
     
     conversation.addMessage([
-        { author: p1, text: "¡Hola a todos! ¿Qué tal están?" },
-        { author: p2, text: "Muy bien, ¡gracias!" },
-        { author: p3, text: "Vaya caras largas traen algunos" },
-        { author: p1, text: "Ya ves, hay que animarse ¡Hombre!" },
-        { author: p3, text: "Bueno, bueno, tampoco nos vengamos arriba" },
-        { author: p2, text: "Eh..." },
-        { author: p1, text: "¡Alehop!" },
-        { author: p3, text: "saca wikicheli pa podé gozá, vaamoh a montaaa un wateeekeee" },
+        { author: p3, text: "AAAHaaAAAaaaaaAaaaaaA aAAaaaaAAaaAAAAaAAAaaa AAAAAaAAAaaa",               mode: "ltl" },
+        { author: p1, text: "¡Hola a todos! ¿Qué tal están?",               mode: "wtw" },
+        { author: p2, text: "Muy bien, ¡gracias!",                          mode: "entirely" },
+        { author: p3, text: "Vaya caras largas traen algunos",               mode: "wtw" },
+        { author: p1, text: "Ya ves, hay que animarse ¡Hombre!",             mode: "wtw" },
+        { author: p3, text: "Bueno, bueno, tampoco nos vengamos arriba",     mode: "wyw" },
+        { author: p2, text: "Eh...",                                         mode: "entirely" },
+        { author: p1, text: "¡Alehop!",                                      mode: "wtw" },
+        { author: p3, text: "saca wikicheli pa podé gozá, vaamoh a montaaa un wateeekeee",  mode: "wtw" },
     ]);
 });
 
 // END EVENTS BASE
-
-/*
-var msg = new SpeechSynthesisUtterance();
-
-// atributos
-msg.lang = "es";
-msg.rate = 1;
-msg.pitch = 0;
-msg.volume = 1;
-
-var count = 0;
-//msg.onstart = () => {};
-msg.onend = () => {msg.text = " "+count++; speechSynthesis.speak(msg);}
-*/
